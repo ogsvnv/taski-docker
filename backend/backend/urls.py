e230b5b2dba36c456a526f549fab68c998ab5573
ogsvnv <ogsvnv@gmail.com>
2026-02-09 19:07:34 +0400

    Zalman Git Push: 09.02.2026 19:07:34,87

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -2,8 +2,7 @@ from api import views
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
-from django.conf import settings
-from django.conf.urls.static import static
+
 
 router = routers.DefaultRouter()
 router.register('tasks', views.TaskView, 'task')
@@ -11,4 +10,4 @@ router.register('tasks', views.TaskView, 'task')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
